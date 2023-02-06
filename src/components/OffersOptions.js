3c6190e954cdd08d7c8585c69daf2175ef350ac9
Pawel <nitpaw@gmail.com>
2023-02-06 15:43:24 +0100

    modified icon in OfferOptions

--- src/components/OffersOptions.js
+++ src/components/OffersOptions.js
@@ -6,13 +6,17 @@ const OffersOptions = () => {
 	return (
 		<>
 			{offersOptions.map(offer => {
-				const { key, style, option} = offer;
+				const { key, style, option } = offer
 
-				return <p key={key} className={style}><i className='fas fa-check'></i>{option}</p>
+				return (
+					<p key={key} className={style}>
+						{style === 'unactive' ? null : <i className='fas fa-check'></i>}
+						{option}
+					</p>
+				)
 			})}
 		</>
 	)
 }
 
 export default OffersOptions
-
